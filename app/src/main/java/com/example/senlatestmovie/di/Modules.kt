c8b3ac682611a18bc32d0887d5ac1ae44767e363
siarhei <sergant483@gmail.com>
2022-07-05 13:02:47 +0300

    Bug fix

--- app/src/main/java/com/example/senlatestmovie/di/Modules.kt
+++ app/src/main/java/com/example/senlatestmovie/di/Modules.kt
@@ -5,15 +5,11 @@ import com.example.senlatestmovie.data.database.AppDataBase
 import com.example.senlatestmovie.data.datasource.MovieListDataSource
 import com.example.senlatestmovie.data.datasource.MovieListDataSourceImpl
 import com.example.senlatestmovie.data.usecase.*
-import com.example.senlatestmovie.feature.ISoundRecordFeature
-import com.example.senlatestmovie.feature.SoundRecordFeature
 import com.example.senlatestmovie.presentation.fragment.extendedmovieinfo.ExtendedMovieInfo
 import com.example.senlatestmovie.presentation.fragment.extendedmovieinfo.ExtendedMovieInfoViewModel
 import com.example.senlatestmovie.presentation.fragment.extendedmovieinfo.ExtendedMoviePresenter
 import com.example.senlatestmovie.presentation.fragment.movie.MoviesFragment
 import com.example.senlatestmovie.presentation.fragment.movie.MoviesViewModel
-import com.example.senlatestmovie.presentation.fragment.music.MusicFragment
-import com.example.senlatestmovie.presentation.fragment.music.MusicViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
@@ -23,9 +19,7 @@ private val fragmentModule: Module = module {
     scope<MoviesFragment> {
         viewModel { MoviesViewModel(get(), get(), get(), get()) }
     }
-    scope<MusicFragment> {
-        viewModel { MusicViewModel(get()) }
-    }
+
     scope<ExtendedMovieInfo> {
         viewModel { ExtendedMovieInfoViewModel(get()) }
         factory { ExtendedMoviePresenter() }
@@ -40,7 +34,6 @@ private val dataModule: Module = module {
 
 private val repositoryModule: Module = module {
     single<MovieListDataSource> { MovieListDataSourceImpl(get()) }
-    single<ISoundRecordFeature> { SoundRecordFeature() }
 }
 
 private val useCaseModule: Module = module {
